88a90a4f632ec871927ab04563bfbef6f3e9d7e4
hashxtree <145032760+hashxtree@users.noreply.github.com>
2024-08-17 15:40:33 +0300

    fixes for noncustodial flow

--- client/evm.go
+++ client/evm.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"github.com/lombard-finance/cubesigner-sdk/api"
 	"net/http"
 	"net/url"
 	"strings"
@@ -10,20 +9,8 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (cli *Client) SignEip712(roleId, pubkey string, request *v0.Eip712SignRequest, mfaId, mfaConfirmation *string) (*v0.EvmSignResponse, string, error) {
-	// This follows the convention of other Sign*() functions, minting a new token per request
-	// TODO: session management improvements (here and elsewhere)
-	authResp, err := cli.CreateRoleToken(&v0.CreateTokenRequest{
-		Purpose: "sign taproot",
-		Scopes:  []api.Scope{api.SIGNBTCTAPROOT},
-	}, roleId)
-	if err != nil {
-		return nil, "", errors.Wrap(err, "create role token")
-	}
-
-	headers := map[string]string{
-		"Authorization": authResp.GetToken(),
-	}
+func (cli *Client) SignEip712(pubkey string, request *v0.Eip712SignRequest, mfaId, mfaConfirmation *string) (*v0.EvmSignResponse, string, error) {
+	headers := map[string]string{}
 
 	// add mfa headers
 	if mfaConfirmation != nil && *mfaConfirmation != "" {
